7b55cbf1def06c4fe665be46e541eb87a54e0855
Mahdan Ahmad Fauzi Al-Hasan <mahdan.ahmad@yahoo.com>
2016-04-15 22:08:11 +0700

    add gitignore

--- stimulus/solid.py
+++ stimulus/solid.py
@@ -1,6 +1,6 @@
 from env import Env
 from pygame.locals import *
-from rectangle import Rectangle
+
 import pygame, threading, sys
 
 pygame.init()
@@ -17,21 +17,14 @@ screen.fill(clr_back)
 def eventLoop() :
     for event in pygame.event.get():
         if event.type == QUIT :
-            env.killStop()
             pygame.quit()
             sys.exit()
         elif event.type == KEYDOWN :
             if event.key == K_ESCAPE :
-                env.killStop()
                 pygame.quit()
                 sys.exit()
-            if event.key == K_SPACE :
-                env.changeRun()
 
 def run() :
-    forward = Rectangle()
-    forward.start(env.getStop(), env.getRun())
-
     while True :
         eventLoop()
         clock.tick(env.getFPS())
