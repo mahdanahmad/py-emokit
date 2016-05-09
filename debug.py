f22efb43766afcdbed0ae1b2ec3cdf947b7b67e8
Mahdan Ahmad Fauzi Al-Hasan <mahdan.ahmad@yahoo.com>
2016-05-09 09:11:16 +0700

    edit figure

--- debug.py
+++ debug.py
@@ -22,8 +22,8 @@ try :
 except:
     # source      = 'data/20160503/174424_p300_stop_20.csv'
     # source      = 'data/20160503/175805_p300_forward_20.csv'
-    source      = 'data/20160503/180853_p300_left_20.csv'
-    # source      = 'data/20160503/181835_p300_right_20.csv'
+    # source      = 'data/20160503/180853_p300_left_20.csv'
+    source      = 'data/20160503/181835_p300_right_20.csv'
 
 def readFromFile(filename) :
     with open(filename) as afile :
@@ -53,74 +53,45 @@ def countDiff(data) :
 def run() :
     start_time      = time.time()
 
-    direction       = source.split('_')[2]
-    fullpath        = os.path.join('result', 'dump_' + direction + '.csv')
-
-    if not os.path.exists(os.path.dirname(fullpath)):
-        try:
-            os.makedirs(os.path.dirname(fullpath))
-        except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
-
-    output          = open(fullpath, 'w')
-    output.write("F3,FC5,AF3,F7,T7,P7,O1,O2,P8,T8,F8,AF4,FC6,F4\n")
-
     data            = readFromFile(source)
     timestamp       = data[:,0]
 
     stimulus_out    = loadStimulus(5.0)
     stimulus        = findStimulus(timestamp, stimulus_out)
 
-    stimulus_single = (int)(sampling_rate / 3)
+    stimulus_single = 0
     sampling        = (1000 / sampling_rate)
-    for key, val in enumerate(stimulus) :
-        first_cut       = val - stimulus_single
-        second_cut      = val + sampling_rate
+    first_cut       = stimulus[4] - stimulus_single
+    second_cut      = stimulus[4] + (sampling_rate / 2)
 
-        if (first_cut < 0) : first_cut = 0
+    current         = moveToAxis(data[:,15])
 
-        for i in range(2, 16) :
-            current         = moveToAxis(data[:,i])
+    if (len(current) >= second_cut) :
+        first_stimulus  = current[first_cut:second_cut]
+        x               = np.arange(len(first_stimulus))
 
-            if (len(current) >= second_cut) :
-                first_stimulus  = current[first_cut:second_cut]
-                x               = np.arange(len(first_stimulus))
+        plt.plot((x - stimulus_single) * sampling , first_stimulus)
 
-                plt.plot((x - stimulus_single) * sampling , first_stimulus)
+        plt.xlabel('Time [ms]')
+        plt.ylabel('Voltage')
 
-                plt.xlabel('Time [ms]')
-                plt.ylabel('Power')
+        plt.axvline(x=0, color='r', ls='--')
 
-                # plt.axvline(x=(stimulus_single * sampling), color='r', ls='--')
-                # plt.axvline(x=((stimulus_single * sampling) + 140), color='g', ls='--')
-                # plt.axvline(x=((stimulus_single * sampling) + 500), color='g', ls='--')
-                plt.axvline(x=0, color='r', ls='--')
-                plt.axvline(x=140, color='g', ls='--')
-                plt.axvline(x=500, color='g', ls='--')
+        parsed      = parse(first_stimulus, split_amount)
+        power       = countAllPower(parsed)
+        diff        = findDifference(power)
+        # print diff
 
-                first_base      = (int)(stimulus_single + math.floor(sampling_rate * 0.14))
-                end_game        = (int)(stimulus_single + math.ceil(sampling_rate * 0.50))
+        parseLine   = np.arange(0, (sampling_rate / 2), split_amount)
+        for key, val in enumerate(parseLine[1:]) :
+            text_axis   = (val - (split_amount / 2)) * sampling
+            text_ord    = np.amax(parsed[key:key+1])
 
-                suspectedPower  = countPower(first_stimulus[first_base:end_game])
-                totalPower      = countPower(first_stimulus[stimulus_single:end_game])
+            plt.axvline(x=(val * sampling), color='g', ls='--')
+            if (key < len(diff)) : plt.text(text_axis, text_ord, "{0:.2f}%".format(diff[key]))
 
-                percentage      = (suspectedPower * 100) / totalPower
-                # print percentage
-
-                plt.title("{0:.2f}%".format(percentage))
-
-                if (i < 15) :
-                    output.write("{0:.2f},".format(percentage))
-                    plt.figure()
-                else :
-                    output.write("{0:.2f}\n".format(percentage))
-                    plt.show()
-
-                # print len(first_stimulus[first_base:end_game])
-                # print len(first_stimulus[stimulus_single:end_game])
-
-    # plt.show()
+        plt.ylim((650,820))
+        plt.show()
 
     elapsed_time    = time.time() - start_time
     print 'elapsed = %.3f s' % (elapsed_time)
