339628cb19c379fad23ad77f51181e99bd6f341f
Mahdan Ahmad Fauzi Al-Hasan <mahdan.ahmad@yahoo.com>
2016-05-15 19:57:44 +0700

    lazy plot with new data

--- debug.py
+++ debug.py
@@ -1,89 +1,13 @@
-import os
-import sys
-import time
-import math
-import random
-import gevent
+import os, sys, time, math, random, gevent
 import numpy as np
 import matplotlib.pyplot as plt
 
-from env import *
-from datetime import datetime
 from preprocess import *
 
-low_limit       = 5
-high_limit      = 20
-sampling_rate   = 128
-split_amount    = 6
-
-header          = ["F3","FC5","AF3","F7","T7","P7","O1","O2","P8","T8","F8","AF4","FC6","F4"]
-
-try :
-    source      = sys.argv[1]
-except:
-    source      = 'data/20160513/184727_Arif_5.csv'
-
-def readFromFile(filename) :
-    with open(filename) as afile :
-        result = []
-        for line in afile :
-            # print line.split(',')
-            splittedLine    = line.split(',')
-            result.append([float(splittedLine[0])] +  map(int, splittedLine[1::1]))
-
-        return np.array(result)
-
-def loadStimulus(diff=0)  :
-    with open('data/stimulus_out.csv') as afile :
-        result  = []
-        for line in afile :
-            result.append(float(line) - diff)
-
-        return np.array(result)
-
-def loadNewStimulus(source) :
-    stimulusPath    = 'result/stimulus' + source.replace('data', '')
-    with open(stimulusPath) as afile :
-        result  = {
-            'time'      : [],
-            'direction' : []
-        }
-        for line in afile :
-            splittedLine    = line.rstrip().split(',')
-            result['time'].append(int(splittedLine[0]))
-            result['direction'].append(splittedLine[1])
-
-        return result
+header      = ["F3","FC5","AF3","F7","T7","P7","O1","O2","P8","T8","F8","AF4","FC6","F4"]
 
 def run() :
     start_time      = time.time()
-
-    data            = readFromFile(source)
-    timestamp       = data[:,0]
-    stimulus_list   = loadNewStimulus(source)
-    stimulus_pos    = findStimulus(timestamp, stimulus_list['time'])
-
-    first_cut       = stimulus_pos[0] - 15
-    second_cut      = stimulus_pos[0] + sampling_rate
-
-    iteree          = range(2, 16)
-    for idx, i in enumerate(iteree) :
-        current     = moveToAxis(data[:,i][first_cut:second_cut])
-        x           = timestamp[first_cut:second_cut] - timestamp[stimulus_pos[0]]
-
-        plt.plot(x, current)
-
-        plt.xlabel('Time [ms]')
-        plt.ylabel('Voltage')
-
-        plt.axvline(x=0, color='r', ls='--')
-        plt.axvline(x=140, color='g', ls='--')
-        plt.axvline(x=500, color='g', ls='--')
-
-        if (idx < (len(iteree) - 1)) :
-            plt.figure()
-        else :
-            plt.show()
 
     elapsed_time    = time.time() - start_time
     print 'elapsed = %.3f s' % (elapsed_time)
--- scripts/lazy-plot-p300.py
+++ scripts/lazy-plot-p300.py
@@ -1,22 +1,22 @@
-import os, sys, math, random
+import os, sys, time, math, random, gevent
 import numpy as np
+import matplotlib.pyplot as plt
 
 from PIL import Image, ImageFont, ImageDraw
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from preprocess import *
 
+folders     = ['data/20160513']
+
 header      = ["F3","FC5","AF3","F7","T7","P7","O1","O2","P8","T8","F8","AF4","FC6","F4"]
 
 font        = ImageFont.truetype("FreeSans.ttf", 28)
-filepath    = 'images/coalesce/base.jpg'
-# base        = Image.open(filepath)
+imagepath   = 'images/coalesce/base.jpg'
 
-sourceList  = [
-    'data/20160503/174424_p300_stop_20.csv',
-    'data/20160503/175805_p300_forward_20.csv',
-    'data/20160503/180853_p300_left_20.csv',
-    'data/20160503/181835_p300_right_20.csv'
-]
+files_amn   = 10
+stimuli_amn = 20
+grouped_val = True
+
 first_base  = 18
 home_run    = 65
 radius      = 26
@@ -41,10 +41,13 @@ position    = [
     [352, 210]  # F4
 ]
 
+dir_counter = { 'left' : 0, 'right' : 0, 'forward' : 0, 'stop' : 0 }
+
 def readFromFile(filename) :
     with open(filename) as afile :
         result = []
         for line in afile :
+            # print line.split(',')
             splittedLine    = line.split(',')
             result.append([float(splittedLine[0])] +  map(int, splittedLine[1::1]))
 
@@ -58,65 +61,167 @@ def loadStimulus(diff=0)  :
 
         return np.array(result)
 
-def run(source):
-    # source          = sourceList[0]
-    direction       = source.split('_')[2]
+def loadStimuli(source) :
+    stimuliPath = 'result/stimulus' + source.replace('data', '')
+    with open(stimuliPath) as afile :
+        result  = {
+            'time'      : [],
+            'direction' : []
+        }
+        for line in afile :
+            splittedLine    = line.rstrip().split(',')
+            result['time'].append(int(splittedLine[0]))
+            result['direction'].append(splittedLine[1])
 
-    data            = readFromFile(source)
-    timestamp       = data[:,0]
+        return result
 
-    stimulus_out    = loadStimulus(5.0)
-    stimulus        = findStimulus(timestamp, stimulus_out)
+def findFiles(folderpath, grouped=False) :
+    result  = []
+    for current_dir in folderpath :
+        for file in os.listdir(current_dir):
+            result.append(current_dir + '/' + file)
 
-    for idx, value in enumerate(stimulus) :
-        iteree      = range(2, 16)
+    if grouped :
+        grouped_result  = {}
+        for val in result :
+            name    = val.split('_')[1]
 
-        canvas      = Image.open(filepath)
-        addition    = ImageDraw.Draw(canvas)
+            if name not in grouped_result : grouped_result[name] = []
+            grouped_result[name].append(val)
 
-        if ((value + home_run) <= len(data[:,0])) :
-            for key, val in enumerate(iteree) :
-                current         = moveToAxis(data[:,val][value:(value+home_run)])
-                suspectedMax    = max(current[first_base:home_run])
-                averageBefore   = np.average(current[first_base:home_run])
-                percentageDiff  = countPercentageDifferent(suspectedMax, averageBefore)
+        return grouped_result
+    else :
+        return result
 
-                if (percentageDiff > 200) :
-                    fill_value  = (0,255,0)
-                elif (percentageDiff > 100) :
-                    green_value = int(math.ceil((percentageDiff - 100) * 2)) + 55
-                    fill_value  = (0,green_value,0)
-                else :
-                    red_value   = int(math.ceil((100 - percentageDiff) * 2)) + 55
-                    fill_value  = (red_value,0,0)
+def randomPick(fileList, count=10, grouped=False) :
+    if grouped :
+        result  = []
+        each    = int(math.ceil(count / len(fileList)))
+        for key, val in fileList.iteritems() :
+            if (len(val) > each) :
+                temp    = random.sample(val, each)
+            else :
+                temp    = val
 
-                # if (percentageDiff > 255) :
-                #     clr_value   = 255
-                # else :
-                #     clr_value   = (int)(math.ceil(percentageDiff))
+            result += temp
 
-                if (percentageDiff < 99) :
-                    text_pos    = (position[key][0] - (radius * 0.6), position[key][1] - (radius * 0.6))
-                else :
-                    text_pos    = (position[key][0] - (radius * 0.9), position[key][1] - (radius * 0.6))
+        return result
+    else :
+        if (len(fileList) > count) :
+            return random.sample(fileList, count)
+        else :
+            return fileList
 
-                addition.ellipse((position[key][0] - (radius + outline), position[key][1] - (radius + outline), position[key][0] + (radius + outline), position[key][1] + (radius + outline)), fill=line_color)
-                addition.ellipse((position[key][0] - radius, position[key][1] - radius, position[key][0] + radius, position[key][1] + radius), fill=fill_value)
-                addition.text(text_pos, str((int)(math.ceil(percentageDiff))), font=font, fill=text_color)
+def randomStimulus(stimuli, count=10, grouped=False) :
+    if grouped :
+        grouped     = {}
+        for key, val in enumerate(stimuli['direction']) :
+            if val not in grouped : grouped[val] = []
+            grouped[val].append(stimuli['time'][key])
 
-            del addition
+        result      = {
+            'time'      : [],
+            'direction' : []
+        }
+        each        = int(math.ceil(count / len(grouped)))
+        for key, val in grouped.iteritems() :
+            if (len(val) > each) :
+                temp    = random.sample(val, each)
+            else :
+                temp    = val
 
-            destination = "result/dump/" + direction + "_" + str(idx + 0) + ".jpg"
+            for ran in temp :
+                result['time'].append(ran)
+                result['direction'].append(key)
 
-            if not os.path.exists(os.path.dirname(destination)):
-                try:
-                    os.makedirs(os.path.dirname(destination))
-                except OSError as exc: # Guard against race condition
-                    if exc.errno != errno.EEXIST:
-                        raise
+    else :
+        converted   = {}
+        for key, val in enumerate(stimuli['time']) : converted[val] = stimuli['direction'][key]
+        randomed    = random.sample(converted.items(), count)
 
-            canvas.save(destination, "JPEG", quality=100, optimize=True, progressive=True)
+        result  = {
+            'time'      : [],
+            'direction' : []
+        }
+        for val in randomed :
+            result['time'].append(val[0])
+            result['direction'].append(val[1])
+
+    sorted_result   = zip(*sorted(zip(result['time'], result['direction'])))
+    return {
+        'time'      : sorted_result[0],
+        'direction' : sorted_result[1]
+    }
+
+def saveImage(canvas, direction) :
+    output_path = "result/dump/" + direction + "_" + str(dir_counter[direction]) + ".jpg"
+    dir_counter[direction] += 1
+
+    print output_path
+
+    if not os.path.exists(os.path.dirname(output_path)):
+        try:
+            os.makedirs(os.path.dirname(output_path))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+    canvas.save(output_path, "JPEG", quality=100, optimize=True, progressive=True)
+
+def putChannel(canvas, data, stimulus_pos, iteree, direction) :
+    addition        = ImageDraw.Draw(canvas)
+
+    if ((stimulus_pos + home_run) <= len(data[:,0])) :
+        for key, val in enumerate(iteree) :
+            current         = moveToAxis(data[:,val][stimulus_pos:(stimulus_pos + home_run)])
+            suspectedMax    = max(current[first_base:home_run])
+            averageBefore   = np.average(current[first_base:home_run])
+            percentageDiff  = countPercentageDifferent(suspectedMax, averageBefore)
+
+            if (percentageDiff > 200) :
+                fill_value  = (0,255,0)
+            elif (percentageDiff > 100) :
+                green_value = int(math.ceil((percentageDiff - 100) * 2)) + 55
+                fill_value  = (0,green_value,0)
+            else :
+                red_value   = int(math.ceil((100 - percentageDiff) * 2)) + 55
+                fill_value  = (red_value,0,0)
+            if (percentageDiff < 99) :
+                text_pos    = (position[key][0] - (radius * 0.6), position[key][1] - (radius * 0.6))
+            else :
+                text_pos    = (position[key][0] - (radius * 0.9), position[key][1] - (radius * 0.6))
+
+            addition.ellipse((position[key][0] - (radius + outline), position[key][1] - (radius + outline), position[key][0] + (radius + outline), position[key][1] + (radius + outline)), fill=line_color)
+            addition.ellipse((position[key][0] - radius, position[key][1] - radius, position[key][0] + radius, position[key][1] + radius), fill=fill_value)
+            addition.text(text_pos, str(int(math.ceil(percentageDiff))), font=font, fill=text_color)
+
+        del addition
+
+        saveImage(canvas, direction)
+
+def run() :
+    start_time      = time.time()
+
+    files           = findFiles(folders, grouped_val)
+    randomed        = randomPick(files, files_amn, grouped_val)
+
+    for filepath in randomed :
+        data        = readFromFile(filepath)
+        timestamp   = data[:,0]
+
+        stimuli     = loadStimuli(filepath)
+        stimuli_ran = randomStimulus(stimuli, stimuli_amn, grouped_val)
+        stimuli_pos = findStimulus(timestamp, stimuli_ran['time'])
+
+        for stimulus_idx, stimulus_pos in enumerate(stimuli_pos) :
+            iteree      = range(2, 16)
+
+            canvas      = Image.open(imagepath)
+            putChannel(canvas, data, stimulus_pos, iteree, stimuli_ran['direction'][stimulus_idx])
+
+    elapsed_time    = time.time() - start_time
+    print 'elapsed = %.3f s' % (elapsed_time)
 
 if __name__ == "__main__":
-    for source in sourceList : run(source)
-    # run()
+    run()
+    # print env_serial\
